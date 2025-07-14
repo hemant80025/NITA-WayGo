357d284a0f6b7cef4bd2c09f8dcd734d49126380
Roushan6207 <roushanrambo@gmail.com>
2025-07-14 14:01:08 +0530

    completed hooks part

---
+++ frontend/src/hooks/useUrlPosition.js
@@ -0,0 +1,11 @@
+
+import { useSearchParams } from "react-router-dom";
+
+export function useUrlPosition() {
+  const [searchParams] = useSearchParams();
+  const id = searchParams.get("_id");
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+
+  return [lat, lng, id];
+}
