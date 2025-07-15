5c032616314cfe56babeeccc7d4b8d0bf269622b
Hemant Kumar <hemantkumar@MacBookAir.lan>
2025-07-16 01:33:21 +0530

    Add navigation and list components to frontend

---
+++ frontend/src/components/AllList.js
@@ -0,0 +1,15 @@
+import Spinner from "./Spinner";
+import { usePlaces } from "../contexts/PlacesContext";
+import List from "./List";
+
+function AllList() {
+  const { places, isLoading } = usePlaces();
+
+  // console.log(searchedPlaces);
+
+  if (isLoading) return <Spinner />;
+  if (!places?.length) return <h2>No Places Available !!</h2>;
+  return <List commonLocation={places} />;
+}
+
+export default AllList;
