8564903428b01f0cc31069af54963131cca3e428
Rohitkumar367 <rohitddroid@gmail.com>
2025-07-15 19:21:37 +0530

    done with the contexts

---
+++ frontend/src/contexts/AuthContext.js
@@ -0,0 +1,155 @@
+import { createContext, useContext, useReducer, useState } from "react";
+import { usePlaces } from "./PlacesContext";
+const AuthContext = createContext();
+
+const initialState = {
+    isAdmin: false,
+    user: null,
+    isAuthenticated: false,
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "loginAdmin":
+            return {
+                ...state,
+                isAdmin: true,
+                user: action.payload,
+                isAuthenticated: true,
+            };
+        case "login":
+            return { ...state, user: action.payload, isAuthenticated: true };
+        case "logout":
+            return { initialState };
+        default:
+            throw new Error("Unknown action type");
+    }
+}
+
+// Define the base URL for API requests
+const BASE_URL = import.meta.env.VITE_Render_Url
+
+function AuthProvider({ children }) {
+    const [{ user, isAuthenticated, isAdmin }, dispatch] = useReducer(
+        reducer,
+        initialState
+    );
+    const [reqNav, setReqNav] = useState(false);
+    const { fetchPlaces, getPinLocation } = usePlaces();
+    const [isNotification, setIsNotification] = useState(false);
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    // login function for users
+    async function login(email, password) {
+        const Response = await fetch(`${BASE_URL}api/authUser/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        const res = await Response.json();
+        console.log(res);
+        if (res.success) {
+            localStorage.setItem("token", res.json);
+            dispatch({
+                type: "login",
+                payload: {
+                    email,
+                    password,
+                },
+            });
+            await fetchPlaces();
+            await getPinLocation();
+            alert("Logged In Successfully!");
+        } else {
+            alert(res.msg);
+        }
+    }
+
+    // login function for admins
+    async function loginAdmin(email, password) {
+        const Response = await fetch(`${BASE_URL}api/authAdmin/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email, password }),
+        });
+        const res = await Response.json();
+        console.log(res);
+        if (res.success) {
+            localStorage.setItem("token", res.json);
+            dispatch({
+                type: "loginAdmin",
+                payload: {
+                    email,
+                    password,
+                },
+            });
+            await fetchPlaces();
+            alert("Logged in successfully");
+        } else {
+            alert(res.msg);
+        }
+    }
+
+    // sign up function for users
+    async function createUser(
+        name,
+        email,
+        password,
+        imageUrl = "https://i.pravatar.cc/100?u=zz"
+    ) {
+        console.log(name);
+        const Response = await fetch(`${BASE_URL}api/authUser/createuser`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ name, email, password, imageUrl }),
+        });
+        const res = await Response.json();
+        console.log(res);
+        return res;
+    }
+
+    function logout() {
+        localStorage.removeItem("token");
+        setIsNotification(false);
+        setReqNav(false);
+        dispatch({ type: "logout" });
+    }
+
+    return (
+        <AuthContext.Provider
+            value={{
+                user,
+                isAuthenticated,
+                isAdmin,
+                reqNav,
+                isNotification,
+                setIsNotification,
+                setReqNav,
+                login,
+                loginAdmin,
+                logout,
+                createUser,
+                sidebarOpen,
+                setSidebarOpen,
+            }}
+        >
+            {children}
+        </AuthContext.Provider>
+    );
+}
+
+function useAuth() {
+    const context = useContext(AuthContext);
+    if (context === undefined) {
+        throw new Error("AuthContext was used outside AuthProvider");
+    }
+    return context;
+}
+
+export { useAuth, AuthProvider };
---
+++ frontend/src/contexts/PlacesContext.js
@@ -0,0 +1,418 @@
+import { Icon } from "leaflet";
+import { createContext, useContext, useReducer, useState } from "react";
+const PlacesContext = createContext();
+
+const initialState = {
+    isLoading: false,
+    currentPlace: {},
+    pinLocation: [],
+    error: "",
+    requestedLocations: [],
+};
+
+function reducer(state, action) {
+    switch (action.type) {
+        case "stopLoading":
+            return {
+                ...state,
+                isLoading: false,
+            };
+        case "loading":
+            return {
+                ...state,
+                isLoading: !state.isLoading,
+            };
+        case "places/loaded":
+            return {
+                ...state,
+                isLoading: false,
+            };
+        case "places/deleted":
+            return {
+                ...state,
+                isLoading: false,
+                currentPlace: {},
+            };
+        case "getPinLocation":
+            return {
+                ...state,
+                isLoading: false,
+                pinLocation: action.payload,
+            };
+        case "reqLocation/loaded":
+            return {
+                ...state,
+                isLoading: false,
+                requestedLocations: action.payload,
+            };
+        case "addPinLocation":
+            return {
+                ...state,
+                isLoading: false,
+            };
+        case "rejected":
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload,
+            };
+        default:
+            throw new Error("Action type not defined");
+    }
+}
+
+const BASE_URL = import.meta.env.VITE_Render_Url
+
+function PlacesProvider({ children }) {
+    const [
+        { currentPlace, isLoading, error, pinLocation, requestedLocations },
+        dispatch,
+    ] = useReducer(reducer, initialState);
+    const [places, setPlaces] = useState([]);
+
+    const [searchQuery, setSearchQuery] = useState("");
+    const searchedPlaces =
+        searchQuery.length > 0
+            ? places.filter((place) =>
+                `${place.name} ${place.type}`
+                    .toLowerCase()
+                    .includes(searchQuery.toLowerCase())
+            )
+            : places;
+
+    async function fetchPlaces() {
+        dispatch({ type: "loading" });
+        try {
+            const response = await fetch(`${BASE_URL}api/user/places`, {
+                method: "GET",
+                headers: {
+                    Authorization: localStorage.getItem("token"),
+                },
+            });
+            const res = await response.json();
+            console.log(res.msg);
+            setPlaces(res.msg);
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an error fetching PLaces...",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    async function fetchReqLocations() {
+        dispatch({ type: "loading" });
+        try {
+            const response = await fetch(
+                `${BASE_URL}api/admin/getuserlocationupdates`,
+                {
+                    method: "GET",
+                    headers: {
+                        Authorization: localStorage.getItem("token"),
+                    },
+                }
+            );
+            const res = await response.json();
+            console.log(res.msg);
+            dispatch({ type: "reqLocation/loaded", payload: res.msg });
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an error fetching PLaces...",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    async function addPlace(
+        name,
+        latitude,
+        longitude,
+        type,
+        imageUrl,
+        importantData
+    ) {
+        dispatch({ type: "loading" });
+
+        try {
+            const Response = await fetch(`${BASE_URL}api/admin/addplace`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: localStorage.getItem("token"),
+                },
+                body: JSON.stringify({
+                    name,
+                    latitude,
+                    longitude,
+                    type,
+                    imageUrl,
+                    importantData,
+                }),
+            });
+            const res = await Response.json();
+            console.log(res);
+            dispatch({ type: "loading" });
+            await fetchPlaces();
+            await fetchReqLocations();
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an error Adding Place...",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    async function addPinLocation(name, latitude, longitude, type, message) {
+        dispatch({ type: "loading" });
+        try {
+            const Response = await fetch(`${BASE_URL}api/user/addpinlocation`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: localStorage.getItem("token"),
+                },
+                body: JSON.stringify({ name, latitude, longitude, type, message }),
+            });
+            const res = await Response.json();
+            console.log(res);
+            dispatch({ type: "addPinLocation", payload: res.data });
+            await getPinLocation();
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an error in Pinning Location...",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    async function deleteLocationByAdmin(latitude, longitude) {
+        dispatch({ type: "loading" });
+        try {
+            const Response = await fetch(`${BASE_URL}api/admin/deletePlace`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: localStorage.getItem("token"),
+                },
+                body: JSON.stringify({ latitude, longitude }),
+            });
+            const res = await Response.json();
+            console.log(res);
+            await fetchPlaces();
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an error in Deleting Location...",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    async function pinDeleteByUser(latitude, longitude) {
+        dispatch({ type: "loading" });
+        try {
+            const Response = await fetch(`${BASE_URL}api/user/deletepinlocation`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: localStorage.getItem("token"),
+                },
+                body: JSON.stringify({ latitude, longitude }),
+            });
+            const res = await Response.json();
+            console.log(res);
+            await getPinLocation();
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an error in Deleting Location...",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    async function rejectRequestByAdmin(latitude, longitude) {
+        dispatch({ type: "loading" });
+        try {
+            const Response = await fetch(
+                `${BASE_URL}api/admin/deleteuserlocationupdates`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        "Content-Type": "application/json",
+                        Authorization: localStorage.getItem("token"),
+                    },
+                    body: JSON.stringify({ latitude, longitude }),
+                }
+            );
+            const res = await Response.json();
+            console.log(res);
+            await fetchReqLocations();
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an error in Deleting Requested Location...",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    async function addFeedback(message) {
+        dispatch({ type: "loading" });
+
+        try {
+            const Response = await fetch(`${BASE_URL}api/user/feedback`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: localStorage.getItem("token"),
+                },
+                body: JSON.stringify({ message }),
+            });
+            const res = await Response.json();
+            console.log(res);
+            dispatch({ type: "loading" });
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an error adding Feedback...",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    async function getPinLocation() {
+        console.log("Fetching pin Location");
+        dispatch({ type: "loading" });
+        try {
+            const Response = await fetch(`${BASE_URL}api/user/pinlocations`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: localStorage.getItem("token"),
+                },
+            });
+            const res = await Response.json();
+            console.log(res.msg);
+            dispatch({ type: "getPinLocation", payload: res.msg });
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an getting Pinned Locations...",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    async function reqLocation(name, latitude, longitude, type) {
+        dispatch({ type: "loading" });
+
+        try {
+            const Response = await fetch(`${BASE_URL}api/user/reqplace`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: localStorage.getItem("token"),
+                },
+                body: JSON.stringify({ name, latitude, longitude, type }),
+            });
+            const res = await Response.json();
+            dispatch({ type: "loading" });
+        } catch {
+            dispatch({
+                type: "rejected",
+                payload: "There is an error in requesting Loaction..",
+            });
+        } finally {
+            dispatch({ type: "stopLoading" });
+        }
+    }
+
+    const presentLocationIcon = new Icon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/128/684/684908.png",
+        iconSize: [38, 38],
+    });
+    const departmentLocationIcon = new Icon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/128/2231/2231492.png",
+        iconSize: [38, 38],
+    });
+    const hostelLocationIcon = new Icon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/128/10607/10607354.png",
+        iconSize: [38, 38],
+    });
+    const restaurantLocationIcon = new Icon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/128/8503/8503966.png",
+        iconSize: [38, 38],
+    });
+    const playgroundLocationIcon = new Icon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/128/7153/7153217.png",
+        iconSize: [38, 38],
+    });
+
+    const otherLocationIcon = new Icon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/128/5817/5817144.png",
+        iconSize: [38, 38],
+    });
+
+    const pinLocationIcon = new Icon({
+        iconUrl: "https://cdn-icons-png.flaticon.com/128/149/149059.png",
+        iconSize: [38, 38],
+    });
+
+    return (
+        <PlacesContext.Provider
+            value={{
+                places: searchedPlaces,
+                setPlaces,
+                isLoading,
+                error,
+                searchQuery,
+                setSearchQuery,
+                currentPlace,
+                presentLocationIcon,
+                departmentLocationIcon,
+                hostelLocationIcon,
+                restaurantLocationIcon,
+                playgroundLocationIcon,
+                pinLocationIcon,
+                otherLocationIcon,
+                pinLocation,
+                requestedLocations,
+                pinDeleteByUser,
+                rejectRequestByAdmin,
+                deleteLocationByAdmin,
+                fetchReqLocations,
+                fetchPlaces,
+                addPlace,
+                addPinLocation,
+                reqLocation,
+                addFeedback,
+                getPinLocation,
+            }}
+        >
+            {children}
+        </PlacesContext.Provider>
+    );
+}
+
+function usePlaces() {
+    const context = useContext(PlacesContext);
+    if (context === undefined)
+        throw new Error("UserContext is outside the UsercontextProvider");
+    return context;
+}
+
+export { PlacesProvider, usePlaces };
