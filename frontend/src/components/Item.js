e083dd4aab54516ed3b2a50fa44cbb5f7e21232d
Hemant Kumar <hemantkumar@Hemants-MacBook-Air.local>
2025-07-16 12:26:01 +0530

    add Hostels,item,itemCss components in frontend

---
+++ frontend/src/components/Item.js
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+import styles from "./Item.module.css";
+import { useAuth } from "../contexts/AuthContext.js";
+import { usePlaces } from "../contexts/PlacesContext.js";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  }).format(new Date(date));
+
+function Item({ currentPlace }) {
+  const { name, date, latitude, longitude, _id } = currentPlace;
+  const { isAdmin, isNotification } = useAuth();
+  const { deleteLocationByAdmin } = usePlaces();
+
+  function handleDelete(e) {
+    e.preventDefault();
+    deleteLocationByAdmin(latitude, longitude);
+  }
+
+  return (
+    <li>
+      <Link
+        className={`${styles.placeItem} ${
+          currentPlace._id === _id ? styles["placeItem--active"] : ""
+        }`}
+        to={`${_id}?lat=${latitude}&lng=${longitude}`}
+      >
+        <h3 className={styles.name}>{name}</h3>
+        {/* <time className={styles.date}>({formatDate(date)})</time> */}
+        {isAdmin && !isNotification &&  (
+          <button className={styles.deleteBtn} onClick={handleDelete}>
+            x
+          </button>
+        )}
+      </Link>
+    </li>
+  );
+}
+
+export default Item;
