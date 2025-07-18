862baa9edd20428f87f5c70134745cd0b1e31903
Hemant Kumar <hemantkumar@MacBookAir.lan>
2025-07-19 00:06:27 +0530

    bug f

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Homepage";
-import "./app.css";
+import "./App.css";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import ProtectedRoutes from "./components/ProtectedRoutes";
